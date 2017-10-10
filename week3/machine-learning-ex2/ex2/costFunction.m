5470cc5fcc4375d6c72105e374d3fc4aa9929c62
birdbird1117 <dapeng.han.1989@gmail.com>
2017-10-10 20:42:22 +0200

    cost function and gradient

--- week3/machine-learning-ex2/ex2/costFunction.m
+++ week3/machine-learning-ex2/ex2/costFunction.m
@@ -19,9 +19,14 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+%theta = initial_theta;
+g = sigmoid(X*theta);
+temp0 = (y.*(-1)).*log(g);
+temp1 = (y.*(-1)+1).*log((g).*(-1)+1);
 
-
-
+J = 1/m*sum(temp0-temp1);
+% FIXME, needs more understanding
+grad = 1/m*((g-y).'*X);
 
 
 
