0a92ccd9f8acda908c156cd05abb970a7e2430df
birdbird1117 <dapeng.han.1989@gmail.com>
2017-10-21 02:42:06 +0200

    gradient

--- week9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ week9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -40,9 +40,9 @@ Theta_grad = zeros(size(Theta));
 %                     partial derivatives w.r.t. to each element of Theta
 %
 
-
-
-
+J = 1/2*(sum(sum((X*Theta.'.*R - Y).^2)));   
+X_grad = (X*Theta.'.*R - Y)*Theta; % still need R
+Theta_grad = (X*Theta.'.*R - Y).'*X;
 
 
 
